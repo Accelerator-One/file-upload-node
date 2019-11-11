d319405f4198e09d239701936bd1062dc6256528
Aditya Thakur <aditya.thakur.261099@gmail.com>
2019-11-11 19:47:00 +0530

    First commit!

---
+++ README.md
@@ -0,0 +1,10 @@
+# File-Upload-Node.js
+A simple Node.js application to upload file content on your cloud Instance
+
+## Steps:
+  - Launch and run an EC2 instance.
+  - Upload the repository on the instance.
+  - Install all the dependencies for running this node.js application.
+  - Add 'upload' folder with write permission, rest as per the requirements.
+  - The port used here for running this server is 8080.
+  - Run on your browser accordingly to upload files.
---
+++ index.js
@@ -0,0 +1,38 @@
+var http = require('http');
+var formidable = require('formidable');
+var fs = require('fs');
+var path = require('path');
+
+http.createServer(function (req,res)
+{
+  if (req.url == '/fileupload')
+   {
+     var form = new formidable.IncomingForm();
+     form.parse(req, function (err, fields, files)
+      {
+       var oldpath = files.filetoupload.path;
+       //console.log(oldpath);
+       var newpath = path.join("upload/"+files.filetoupload.name);
+       //console.log(newpath);
+       fs.rename(oldpath, newpath, function (err) {
+        if (err) throw err;
+          res.write('File uploaded and moved! You can close this window.');
+        res.end();
+      });
+  });
+}
+   else
+  {
+    // Reading the main index file
+    fs.readFile('index.html',function(err,data){
+      res.writeHead(200,{'Content-Type':'text/html'});
+
+      if(err) throw err;
+      console.log("Read successful!");
+
+      //Render read file as Output 
+      res.write(data);
+      res.end();
+  });
+}  
+}).listen(8080); 
